e885cdbecc0aa2681da6f55d3202c83085c67e50
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-06 16:39:48 +0530

    Create camelcase.c

---
+++ camelcase.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{char a[100];
+int i,j;
+printf("\nEnter string");
+    scanf("%[^\n]s",a);
+    a[0]=a[0]-32;
+    for(i=0;a[i]!='\0';i++)
+    {
+    if(a[i]==' ')
+    a[i+1]=a[i+1]-32;
+    }
+printf("Camel case:%s",a);
+    return 0;
+}
