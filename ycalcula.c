be5275b3f3bfeb192612507cc831ad7cbcd44c68
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-16 15:02:50 +0530

    Create ycalcula.c

---
+++ ycalcula.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int a,b,d,A,B,y,x,X;
+  printf("\nEnter A:");
+  scanf("%d",&A);
+  printf("\nEnter B:");
+  scanf("%d",&B);
+  printf("\nEnter X:");
+  scanf("%d",&X);
+  a=A;
+  b=B;
+  x=X;
+  y=a*x+b;
+  printf("y=%d",y);
+  
+  
+}
